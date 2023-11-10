a4592e0f0aeeaf8f84ccf3f1a04ec10480725723
JalesiaGriffin <jgriffin@appdev.yearup.org>
2023-11-10 13:19:07 -0500

    initial commit

--- src/main/java/com/pluralsight/LeaseContract.java
+++ src/main/java/com/pluralsight/LeaseContract.java
@@ -1,2 +1,45 @@
-package com.pluralsight;public class LeaseContract {
+package com.pluralsight;
+
+public class LeaseContract extends Contract{
+    private double expectedEndingValue;
+    private double leaseFee;
+
+    public LeaseContract(String date, String name, String email, Vehicle vehicle) {
+        super(date, name, email, vehicle);
+        this.expectedEndingValue = expectedEndingValue;
+        this.leaseFee = leaseFee;
+    }
+
+    public double getExpectedEndingValue() {
+        expectedEndingValue = getVehicle().getPrice() * 0.5;
+        return expectedEndingValue;
+    }
+
+    public void setExpectedEndingValue(double expectedEndingValue) {
+        this.expectedEndingValue = expectedEndingValue;
+    }
+
+    public double getLeaseFee() {
+        leaseFee = getVehicle().getPrice() * 0.07;
+        return leaseFee;
+    }
+
+    public void setLeaseFee(double leaseFee) {
+        this.leaseFee = leaseFee;
+    }
+
+    @Override
+    public double getTotalPrice() {
+        return (getVehicle().getPrice() - expectedEndingValue) + leaseFee;
+    }
+
+    @Override
+    public double getMonthlyPayment() {
+        int numberOfPayments = 36;
+        double interestRate = 4.0 / 1200;
+        double monthlyPayment = getTotalPrice() * (interestRate * Math.pow(1 + interestRate, numberOfPayments)) / (Math.pow(1 + interestRate, numberOfPayments) - 1);
+        monthlyPayment = Math.round(monthlyPayment * 100);
+        monthlyPayment /= 100;
+        return monthlyPayment;
+    }
 }
--- src/main/java/com/pluralsight/Program.java
+++
@@ -1,8 +0,0 @@
-package com.pluralsight;
-
-public class Program {
-    public static void main(String[] args) {
-        UserInterface ui = new UserInterface();
-        ui.display();
-    }
-}
